da9747e8a80be0982c176c38777f1e888dd941d0
23calva <23calva@live.elida.k12.oh.us>
2020-11-18 10:52:48 -0500

    Player looks around with mouse and player movement added.

--- Assets/Scripts/Camera_Follow.cs
+++ Assets/Scripts/Camera_Follow.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Camera_Follow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
---
+++ Assets/Scripts/Mouse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    public GameObject target;
+    public float speed;
+
+    private void Update()
+    {
+        Cursor.lockState = CursorLockMode.Locked;                              //Hides cursor
+        float horizontal = Input.GetAxis("Mouse X");                           //Gets mouse X input
+        float vertical = Input.GetAxis("Mouse Y");                             //Gets mouse Y input
+        target.transform.Translate(horizontal * speed, 0, vertical * speed);   //Applies the mouse XY inputs to the target
+    }
+}
