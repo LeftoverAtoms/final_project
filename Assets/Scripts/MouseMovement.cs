34bcac0a17f995cc4f028d0004cbc8fe81d5543d
23calva <23calva@live.elida.k12.oh.us>
2020-11-19 10:54:07 -0500

    Stuff

---
+++ Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    public GameObject aim;
+    public float speed;
+
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;                             //Locks cursor
+        Cursor.visible = false;                                               //Hides cursor
+    }
+
+    public void Update()
+    {
+        float x = Input.GetAxis("Mouse X");                                   //Gets mouse X input
+        float y = Input.GetAxis("Mouse Y");                                   //Gets mouse Y input
+        aim.transform.Translate(x * speed, 0, y * speed);                     //Applies inputs to the target prefab
+    }
+
+    public void FixedUpdate()
+    {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);   //Keeps mouse in view
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public GameObject target;
     public GameObject player;
+    public GameObject aim;
     public float speed;
 
     public void Update()
     {
+        player.transform.LookAt(aim.transform);
+    }
+
+    public void FixedUpdate()
+    {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         player.transform.Translate(x * speed, 0, y * speed, Space.World);
-        player.transform.LookAt(target.transform);
     }
 }
