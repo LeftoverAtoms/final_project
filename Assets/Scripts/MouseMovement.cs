ab08ed7bcaa9fbb044111be7cee42f0da2c452f9
LeftoverAtoms <leftoverenchilada@gmail.com>
2020-11-20 00:26:54 -0500

    Mouse game object will follow player .

--- Assets/Scripts/MouseMovement.cs
+++ Assets/Scripts/MouseMovement.cs
@@ -4,20 +4,15 @@ using UnityEngine;
 
 public class MouseMovement : MonoBehaviour
 {
+    public GameObject player;
     public GameObject aim;
-    public float speed;
+    public float mSpeed;
+    public float kbSpeed;
 
     public void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;                             //Locks cursor
         Cursor.visible = false;                                               //Hides cursor
-    }
-
-    public void Update()
-    {
-        float x = Input.GetAxis("Mouse X");                                   //Gets mouse X input
-        float y = Input.GetAxis("Mouse Y");                                   //Gets mouse Y input
-        aim.transform.Translate(x * speed, 0, y * speed);                     //Applies inputs to the target prefab
     }
 
     public void FixedUpdate()
@@ -26,5 +21,13 @@ public class MouseMovement : MonoBehaviour
         pos.x = Mathf.Clamp01(pos.x);
         pos.y = Mathf.Clamp01(pos.y);
         transform.position = Camera.main.ViewportToWorldPoint(pos);
+
+        float kbx = Input.GetAxis("Horizontal");                              //Gets keyboard X input
+        float kby = Input.GetAxis("Vertical");                                //Gets keyboard Z input
+
+        float x = Input.GetAxis("Mouse X");                                   //Gets mouse X input
+        float y = Input.GetAxis("Mouse Y");                                   //Gets mouse Z input
+        aim.transform.Translate(x * mSpeed, 0, y * mSpeed);                   //Applies mouse inputs
+        aim.transform.Translate(kbx * kbSpeed, 0, kby * kbSpeed);             //Applies keyboard inputs
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,13 +10,13 @@ public class Player : MonoBehaviour
 
     public void Update()
     {
-        player.transform.LookAt(aim.transform);
+        player.transform.LookAt(aim.transform);                             //Makes the player always look at the target
     }
 
     public void FixedUpdate()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-        player.transform.Translate(x * speed, 0, y * speed, Space.World);
+        float x = Input.GetAxis("Horizontal");                              //Gets keyboard X input
+        float y = Input.GetAxis("Vertical");                                //Gets keyboard Z input
+        player.transform.Translate(x * speed, 0, y * speed, Space.World);   //Applies keyboard inputs
     }
 }
