2d12e59689ef01983c341ec071f0e18c3cfcc261
23calva <23calva@live.elida.k12.oh.us>
2020-11-20 10:53:08 -0500

    Added enemy AI.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,8 +11,11 @@ public class Player : MonoBehaviour
     public void FixedUpdate()
     {
         player.transform.LookAt(aim.transform);                             //Makes the player always look at the target
+
         float x = Input.GetAxis("Horizontal");                              //Gets keyboard X input
         float y = Input.GetAxis("Vertical");                                //Gets keyboard Z input
         player.transform.Translate(x * speed, 0, y * speed, Space.World);   //Applies keyboard inputs
     }
+
+    public void
 }
