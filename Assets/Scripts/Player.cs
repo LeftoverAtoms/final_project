a9e78808d474abf502941202678ce80af1100907
23calva <23calva@live.elida.k12.oh.us>
2020-11-23 10:51:39 -0500

    Update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,12 +10,16 @@ public class Player : MonoBehaviour
 
     public void FixedUpdate()
     {
-        player.transform.LookAt(aim.transform);                             //Makes the player always look at the target
+        player.transform.LookAt(aim.transform);                                          //Makes the player always look at the target
 
-        float x = Input.GetAxis("Horizontal");                              //Gets keyboard X input
-        float y = Input.GetAxis("Vertical");                                //Gets keyboard Z input
-        player.transform.Translate(x * speed, 0, y * speed, Space.World);   //Applies keyboard inputs
+        float x = Input.GetAxis("Horizontal");                                           //Gets keyboard X input
+        float y = Input.GetAxis("Vertical");                                             //Gets keyboard Z input
+        player.transform.Translate(x * speed, 0, y * speed, Space.World);                //Applies keyboard inputs
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Physics.Raycast(player.transform.position, player.transform.forward, 50);
+            Debug.DrawLine(player.transform.position, aim.transform.position);
+        }
     }
-
-    public void
 }
