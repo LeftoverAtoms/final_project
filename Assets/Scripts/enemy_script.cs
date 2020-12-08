fe57a1a0c792f8d5c4778e55aeec248487e5f97a
23calva <23calva@live.elida.k12.oh.us>
2020-12-08 10:52:56 -0500

    Spawn fixes.

--- Assets/Scripts/enemy_script.cs
+++ Assets/Scripts/enemy_script.cs
@@ -5,62 +5,19 @@ using UnityEngine.AI;
 
 public class enemy_script : MonoBehaviour
 {
-    private GameObject instantiate_enemy;
     public GameObject player;
     public GameObject enemy;
 
-    //private NavMeshAgent agent;
-
-    public float currentTime;
-    public float spawn_rate = 1;
-
-    public bool has_spawned;
-    private bool round_end;
+    private NavMeshAgent agent;
 
     void Start() //CALLED AT START
     {
         player = GameObject.FindWithTag("Player");
-        //agent = enemy.GetComponent<NavMeshAgent>();
+        agent = enemy.GetComponent<NavMeshAgent>();
     }
 
     void FixedUpdate() //UPDATES EVERY FRAME
     {
-        EnemyAI(); //ENEMY AI
-        EnemySpawn(); //ENEMY SPAWNER
-    }
-
-    //ENEMIES SPAWN WITHOUT CARING ABOUT CURRENTTIME
-
-    void EnemySpawn()
-    {
-        if (Input.GetKey(KeyCode.Mouse1) && has_spawned == false)
-        {
-            instantiate_enemy = Instantiate(enemy, new Vector3(0, 0, 0), transform.rotation);
-            has_spawned = true;
-            Debug.Log("ENEMY SPAWNED");
-        }
-
-        if (has_spawned == true)
-        {
-            currentTime = currentTime + 1 * Time.deltaTime;
-            if (currentTime > spawn_rate)
-            {
-                has_spawned = false;
-                currentTime = 0;
-            }
-        }
-    }
-
-    //void RoundSystem() //ROUND LOGIC
-    //{
-    //    if (round_end)
-    //    {
-    //        enemy_count = enemy_count * 0.5f;
-    //    }
-    //}
-
-    void EnemyAI() //ENEMY AI
-    {
-        //agent.destination = player.transform.position;
+        agent.destination = player.transform.position;
     }
 }
---
+++ Assets/Scripts/entity_spawn_script.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class entity_spawn_script : MonoBehaviour
+{
+    private GameObject instantiate_enemy;
+    public GameObject enemy;
+
+    private float enemyCurrentTime;
+    public float enemySpawnRate;
+    public float enemyCount;
+    public float enemyMax = 5;
+
+    private bool enemyHasSpawned;
+    public bool roundEnd;
+
+    void FixedUpdate()
+    {
+        EnemySpawn();
+    }
+
+    void EnemySpawn()
+    {
+        if (enemyCount <= enemyMax)
+        {
+            if (enemyHasSpawned == false)
+            {
+                instantiate_enemy = Instantiate(enemy, new Vector3(0, 0, 0), transform.rotation);
+                enemyHasSpawned = true;
+                enemyCount = enemyCount + 1;
+                Debug.Log("ENEMY SPAWNED");
+            }
+
+            if (enemyHasSpawned == true)
+            {
+                enemyCurrentTime = enemyCurrentTime + 1 * Time.deltaTime;
+                if (enemyCurrentTime > enemySpawnRate)
+                {
+                    enemyHasSpawned = false;
+                    enemyCurrentTime = 0;
+                }
+            }
+        }
+        else if (enemyCount >= enemyMax)
+        {
+            return;
+        }
+    }
+}
